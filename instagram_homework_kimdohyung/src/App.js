d1cdae1b2d91141810ea8d12c8944370f75606c4
kkdh1206 <kkdh1206@naver.com>
2023-11-07 21:09:22 +0900

    서버연결 만들었습니다

--- instagram_homework_kimdohyung/src/App.js
+++ instagram_homework_kimdohyung/src/App.js
@@ -4,22 +4,31 @@ import Mypage from './Mypage';
 import { Route, Routes } from 'react-router-dom';
 import EditProfile from './EditProfile';
 import MyBoard from './myBoard';
-import React,{createContext, useState} from 'react';
+import React,{createContext, useEffect, useState} from 'react';
+import axios from 'axios';
+
 
 
 const ProfileContext = createContext();
 const UserStateContext = createContext();
 
+
+
 function App() {
-  const [profile, setProfile] = useState({
-    name:'kkdh1206' ,
-    imageUrl :"https://i.ibb.co/LPK9dGH/image.png" ,
+
+  const [profile, setProfile] =  useState ({
+    
+    name: "",  //  이 정보들도 get으로 받아와야함
+    imageUrl :"https://i.ibb.co/LPK9dGH/image.png",
     text :'안녕하세요',
     web: '',
     gender: 'male',
-    email: 'kkdh1206@gmail.com'
+    email: 'kkdh1206@gmail.com',
+    age: "",
+    part: ""
   });
-
+  
+  
   const [userState, setUserState] = useState({
     follows: '100' ,
     followers: '100' ,
@@ -27,10 +36,47 @@ function App() {
     images : ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpu6vU-z76nkCjeTqAm5UirtDnlsGv8CIm0w&usqp=CAU',
             'https://t1.daumcdn.net/cfile/tistory/9906804C5FB7337315',
             'https://img.hani.co.kr/imgdb/original/2007/1227/68227042_20071227.jpg']
-
+  
   })
 
+  
+  const getData = async()=>{
+    
+    try {
+      const Axiosresponse = await axios.get('http://3.35.236.83/pard/search/김도형');
+      
+      const response = Axiosresponse.data;
+      // console.log(response);
+      // console.log(">>>>");
+      setProfile({
+        name: response.data.name,  //  이 정보들도 get으로 받아와야함
+        imageUrl :response.data.imgURL,
+        text :'안녕하세요',
+        web: '',
+        gender: 'male',
+        email: 'kkdh1206@gmail.com',
+        age: response.data.age,
+        part: response.data.part
+       })
 
+       localStorage.setItem('userName', response.data.name);
+       localStorage.setItem('userImage', response.data.imgURL);
+       localStorage.setItem('userAge', response.data.age);
+       localStorage.setItem('userPart', response.data.part);
+      
+    } catch (error) {
+      console.log('Error fetching notices: ', error);
+    }
+  }
+
+  useEffect(()=>{ // 한번만 수행 되기 위해서 이걸 둠
+    getData();},[] // 배열 끝에 [] 없으면 랜더링이 될때마다 실행된다.
+  );
+ 
+  
+
+  
+  
   return (
     <div className="App">
       <ProfileContext.Provider value ={{ profile, setProfile}}>
--- instagram_homework_kimdohyung/src/EditProfile.js
+++ instagram_homework_kimdohyung/src/EditProfile.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useRef} from 'react';
+import React, { useContext, useState, useRef, useEffect} from 'react';
 import styled from "styled-components";
 import logoImage from './Icons/Logo.png';
 import homeImage from './Icons/home.png';
@@ -6,60 +6,148 @@ import menuImage from './Icons/noHome.png';
 import textImage from './Icons/text.png';
 import { ProfileContext } from './App';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
 
 function EditProfile(){
     const {profile, setProfile} = useContext(ProfileContext); // useContext는 중괄호로 묶어야 문제없고 대괄호는 에러뜸
     
-    const fileInputRef = useRef(null);
-    const [inputValue, setInputValue] = useState(
-        profile
+    const fileInputRef = useRef(null); // DOM 요소에 접근 가능
+    const [inputValue, setInputValue] = useState({
+        name: localStorage.getItem('userName') ,  
+        imageUrl :localStorage.getItem('userImage'),
+        text: profile.text,
+        web: profile.web,
+        gender: profile.gender,
+        email: profile.email,
+        age: localStorage.getItem('userAge'),
+        part: localStorage.getItem('userPart')
+    }
     );
     const [imageUrl, setImageUrl] = useState(inputValue.imageUrl); // useState는 앞에 중괄호로 묶으니까 사진이 안뜸 대괄호는 뜸
 
-    const handleInputChange = (event) => { // 값이 변하면 setInputvalue호출
+    const[able, setAble] = useState(
+        true
+    );
+
+    // let updatedImageUrl; // 이미지 변경해도 setImage 보다 서버로 전송되는게 빨라서 어쩔수 없이 보안 문제 생길지도 모르지만 이렇게 만듬 일단
+    // 이유는 모르겠는데 서버로 보내는 함수안에서 undefined 라고 떠버림
+
+    // useEffect(()=>{},[able]);
+
+    const handleInputChange = async (event) => { // 값이 변하면 setInputvalue호출
         const {name, value} = event.target;
         setInputValue({
-            name: inputValue.name ,
+            name: inputValue.name ,  // 여기 있는놈들은 [name]: value 만하면 이전꺼가 다 지워지게 되버려서그럼
             imageUrl :inputValue.imageUrl,
             text: inputValue.text,
             web: inputValue.web,
             gender: inputValue.gender,
             email: inputValue.email,
+            age: inputValue.age,
+            part: inputValue.part,
             [name]: value  
+            
             // 이렇게 위에 나머지 값들을 계속 저장해주고 마지막에 바뀐값 하나만 반영하면 원래게 위에 정의 되있어도 밑에서 수정됨
-            }); 
-      };
+            }
+            );
 
-    const changeProfile = ()=> { 
-        if(inputValue===profile){
-        }
-        else{
-        setProfile(inputValue);
+            await setAble(false); // 그냥 바로 동작하게 바꿈 --> 근데 이 함수 자체가 비동기 함수가 아니라서 의미가 없다 await 가
+            
+            // if(event.target.value===profile){
+            //      await setAble(true); // 원래 이렇게 하면 바로 랜더링 되야하지만 바로 효율성을 위해서 바로 안하는 경우가 존재
+            // // 그래서 useEffect를 이럴때 사용해도 잘 안되서 비동기를 사용하니 되었다.
+            // }
+            // else{
+            //     await setAble(false);
+            // } 
+            //  // 이러면 able 바뀔때마다 랜더링됨
+      };
+   
+    const changeProfile = async()=> { 
+        
+        setProfile(inputValue);  // 여긷도 setProfile은 동기함수라 await 는 의미가 없다
+
+        // console.log(inputValue.imgURL, "확인 ㄱㄱ");
+        // console.log("Wls", updatedImageUrl);
+        // let Image = await updatedImageUrl;
+        const data = await {
+            // "name": profile.name,  --> 이렇게 처리를 안정적이게 하고 싶으면 setProfile 을 비동기 함수로 만들어줘서
+            // "age": profile.age,
+            // "part": profile.part,
+            // "imgURL": profile.imageurl
+            
+            "name": inputValue.name,
+            "age": inputValue.age,
+            "part": inputValue.part,
+            "imgURL":  localStorage.getItem('ImageUrl') // 전역변수임 이미지 바뀐게 여기 저장되있음
+          };
+          
+          try {
+            const response = await axios.patch(`http://3.35.236.83/pard/update/김도형`, data);
+            
+            console.log('API Response: ', response.data);
+          } catch (error) {
+            console.log('Error fetching notices: ', error);
+          }
+           
+          
+
         alert("정보가 수정되었습니다.");
-    }}
+        await setAble(true);
+        // useEffect(()=>{},[able]); // 이러면 able 바뀔때마다 랜더링됨 - 이건 이 함수 밖에 총 1번만 사용되있음
+    }
+
+    
 
     const handleImageChangeClick = () => { // 만약 useRef를 사용하는게 여러개라면 이 함수의 매개변수에 index 변수(사용할때 map으로 index로 나눠주고 button에서도 받음)를 써서 받아야함
         fileInputRef.current.click();
     };
 
     const handleImageChange = (event) =>  {
-        const file = event.target.files[0]; // 꼭 이렇게 list 형식으로 받아야 하는듯 아님  오류남!!
-        
-        if (file) {
-            const fileUrl = URL.createObjectURL(file); // 내부 파일 인터넷에 업로드해서 주소 쓸수 있게 하는듯
-            setImageUrl (fileUrl);
+        // const file = event.target.files[0]; // 꼭 이렇게 list 형식으로 받아야 하는듯 아님  오류남!!
+        // 기존 꺼는 비주얼 스튜디오에서 제공해주는 서버라 이렇게 보냈는데 파드 서버에 보낼려고 수정함
+
+        const formData = new FormData();
+        formData.append("image", event.target.files[0]); // image 인거 명시해줘서 JSON 으로 보낼라고 image 달아주는듯
+
+
+        // if (file) {
+        //     const fileUrl = URL.createObjectURL(file); // 내부 파일 인터넷에 업로드해서 주소 쓸수 있게 하는듯
+        //     setImageUrl (fileUrl);
+        //     setInputValue((prevInputValue) => ({
+        //         ...prevInputValue, // 기존 inputValue를 복사
+        //         imageUrl: fileUrl, // imageUrl만 변경
+        //     }));
+        axios
+        .post("http://3.35.236.83/image", formData)
+        .then((response) => {
+
+            localStorage.setItem('ImageUrl',response.data) ; // 업데이트 이미지에 할당
+            // console.log("찐", updatedImageUrl);
+
+          console.log("이미지가 성공적으로 업로드되었습니다:", response.data);
+          setImageUrl (response.data); // imageUrl 업데이트
             setInputValue((prevInputValue) => ({
                 ...prevInputValue, // 기존 inputValue를 복사
-                imageUrl: fileUrl, // imageUrl만 변경
+                imageUrl: response.data, // imageUrl만 변경
             }));
-            
-            
-        }
+            // console.log(inputValue.imageUrl);
+            // console.log(imageUrl);
+            // console.log(response.data);
+          // 서버에서의 응답을 처리합니다.
+          setAble(false); // 버튼 색 활성화
+        })
+        .catch((error) => {
+          console.error("이미지 업로드 중 오류 발생:", error);
+          // 오류를 처리합니다.
+        });
+    };
+    
         
         
         
-    }
+        
+    
 
     // const handleCustomButtonClick = () => {
     //     // 사용자 지정 버튼을 클릭하면 실제 파일 선택(input) 버튼을 클릭합니다.
@@ -107,7 +195,7 @@ function EditProfile(){
                     <Head>
                         <ProfileImage src={imageUrl}></ProfileImage>
                         <ProfileContainer>
-                            <Name>{profile.name}</Name>
+                            <Name>{localStorage.getItem('userName')}</Name>
                             <ImageChange onClick={handleImageChangeClick}> 
                                 프로필 사진 변경
                                 <input onChange={handleImageChange} type="file" accept="image/*"
@@ -118,11 +206,10 @@ function EditProfile(){
                         </ProfileContainer>
                     </Head>
                     <InputContaioner>사용자 이름 <Input type='text' name='name' value={inputValue.name} onChange={handleInputChange}></Input></InputContaioner>
-                    <InputContaioner>소개 <Input type='text' name='text' value={inputValue.text} onChange={handleInputChange}></Input></InputContaioner>
-                    <InputContaioner>웹 사이트 <Input type='text' name='web' value={inputValue.web} onChange={handleInputChange}></Input></InputContaioner>
-                    <InputContaioner>이메일 <Input type='text' name='email' value={inputValue.email} onChange={handleInputChange}></Input></InputContaioner>
-                    <InputContaioner>성별 <Input type='text' name='gender' value={inputValue.gender} onChange={handleInputChange}></Input></InputContaioner>
-                    <Submit onClick={changeProfile}> <Text>제출</Text></Submit>
+                    <InputContaioner>나이 <Input type='text' name='age' value={inputValue.age} onChange={handleInputChange}></Input></InputContaioner>
+                    <InputContaioner>파트 <Input type='text' name='part' value={inputValue.part} onChange={handleInputChange}></Input></InputContaioner>
+                    
+                    <Submit primary={able} onClick={changeProfile} disabled={able} > <Text>제출</Text></Submit>
                     {/* 위와 같이 namd을 이용해서 구분해서 각각 따로 넣어준다  
                     이때 name은 실제 이름과 같아야지 json형태의 파일에서 앞에 속성값이 name으로 들어갈때 정확히 들어가서 정확히 저장됨 */}
                 </Main>
@@ -133,6 +220,8 @@ function EditProfile(){
         </Page>
     );
 }
+
+
 
 
 const Text = styled.p`
@@ -196,7 +285,8 @@ line-height: normal;
 `;
 
 const Submit = styled.button`
-    background-color: #0095F6;
+    background-color: ${(props) => props.primary?   'gray':'#0095F6'};
+    
     width: 60px;
     height: 30px;
     flex-shrink: 0;
